683cb0744dbff7111415a8a15bf69290edc46d93
alexp230 <apruitt230@gmail.com>
2023-10-27 11:32:04 -0500

    Added three meals to the Meals database

--- crud.py
+++ crud.py
@@ -2,10 +2,10 @@
 This file is used to create, read, update, and delete data from the database for testing purposes.
 It is not used in the final version of the project.
 """
-from main import db, app, User
+from main import db, app, User, Meal
 
 # Create a new user object
-new_user = User(U_id = 1, fname="John", lname="Doe", email="johndoe@example.com", password="password123", 
+new_user = User(fname="John", lname="Doe", email="johndoe@example.com", password="password123", 
                 address="123 Main St", subscriptionType="basic")
 
 with app.app_context():
@@ -21,4 +21,9 @@ with app.app_context():
         print(f"Deleted {user.fname} {user.lname} from the database.")
         db.session.delete(user)
         db.session.commit()
+
     print(User.query.all())
+    print("\n\n\n")
+    print(Meal.query.all())
+
+    
